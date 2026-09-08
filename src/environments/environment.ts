aef4014ae9f39e4234cba5581f5551c7d1487784
Phuong65 <145210443+Phuong65@users.noreply.github.com>
2026-09-08 09:46:55 +0700

    a

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,5 +5,5 @@ import { DEPLOYMENTS_FOR_DEV } from './deployment';
 export const environment : Environment = {
     appVersion : packageInfo.version ,
     production : false ,
-    deployment : DEPLOYMENTS_FOR_DEV[ 'server_online' ]
+    deployment : DEPLOYMENTS_FOR_DEV[ 'server_dev' ]
 };
